a21bc202f2807ed6a08114fdc764f86c996574c6
luckel92 <129117205+luckel92@users.noreply.github.com>
2023-07-10 14:29:57 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -47,7 +47,9 @@ class Form{
            
               database.ref("/").set({
                   //defina o valor inicial para playerCount,gameState,players
-                 
+                playerCount: 0,
+                gameState: 0,
+                players: 0
                 });
                 //reload() a janela
                 window.location.reload();
